287a9fc43bd83bf03ceb0fcf63766702b9fd68a7
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-02-18 00:57:15 -0300

    rota perguntar/bootstrap

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express();
 
 // Estou falando para o express usar o ejs como view engine
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render("index");
 });
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+})
 
 app.listen(8888, () =>{
     console.log("app rodando!")
